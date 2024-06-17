da98596835cf8d14db39428276a5ac8de2805cdd
arleysouza <arley.ferreira.souza@gmail.com>
2024-06-16 23:53:05 -0300

    codigo

---
+++ src/pages/PostPage.tsx
@@ -0,0 +1,12 @@
+import { Header, Title, Logo } from "../components";
+
+export default function PostPage() {
+  return (
+    <div>
+      <Header>
+        <Title>Prova</Title>
+        <Logo />
+      </Header>
+    </div>
+  );
+}
